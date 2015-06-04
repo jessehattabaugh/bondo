ab54cc69dd503c3a08153c00edbb864aa24365c5
arkanciscan <arkanciscan@gmail.com>
2015-06-04 01:50:06 -0400

    first two tests in new implementation

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 "use strict";
 
+let h = bondo.h = require('virtual-dom/h');
+let diff = require('virtual-dom/diff');
+let patch = require('virtual-dom/patch');
+let createElement = require('virtual-dom/create-element');
+
+module.exports = bondo;
+  
+function bondo(name, view, ...other) {
+  // name is the name of the element that will be registered 
+  // model is a DOM node that will be replaced with the vdom tree returned by the view
+  // the model node will be passed as the first argument to the view function
+  // view is a function that returns a vdom tree
+  // view takes at least one object which represents the model node
+  // view gets called whenever the attributes of the model node change 
+  // ...other are any other values that will be applied as arguments to the view function after the model  
+  
+  let constructor = document.registerElement(name, {
+    prototype: Object.create(HTMLElement.prototype, {
+      createdCallback: {
+        value: function () {
+          console.dir(this);
+          let oldChildren = [];
+          while (this.firstChild) {
+            oldChildren.push(this.firstChild);
+            this.removeChild(this.firstChild);
+          }
+          this.appendChild(createElement(view(this)));
+          /*
+          // 1: Create a function that declares what the DOM should look like
+          function render(count)  {
+              return h('div', {
+                  style: {
+                      textAlign: 'center',
+                      lineHeight: (100 + count) + 'px',
+                      border: '1px solid red',
+                      width: (100 + count) + 'px',
+                      height: (100 + count) + 'px'
+                  }
+              }, [String(count)]);
+          }
+
+          // 2: Initialise the document
+          var count = 0;      // We need some app data. Here we just store a count.
+
+          var tree = render(count);               // We need an initial tree
+          var rootNode = createElement(tree);     // Create an initial root DOM node ...
+          document.body.appendChild(rootNode);    // ... and it should be in the document
+          
+          // 3: Wire up the update logic
+          setInterval(function () {
+                count++;
+
+                var newTree = render(count);
+                var patches = diff(tree, newTree);
+                rootNode = patch(rootNode, patches);
+                tree = newTree;
+          }, 1000);
+          */
+        }
+      }
+    })
+  });
+  
+  return constructor;
+}
+
+/*
 module.exports = function (element, model) {
   let walk = require('dom-walk');
   let id = (typeof element == 'string') ? element : element.localName;
@@ -51,3 +118,4 @@ module.exports = function (element, model) {
   
   return node;
 };
+*/
--- test/test.js
+++ test/test.js
@@ -1,7 +1,31 @@
-var test = require('tape');
-var bondo = require('../index.js');
+const bondo = require('../index.js');
 require('object.observe');
 
+const test = require('tape');
+
+// 1
+test("Exports virtual hypserscript on h property", function (t) {
+  t.plan(1);
+  let h = bondo.h;
+  //console.dir(h('div'));
+  t.equal(h('div').tagName, 'DIV');
+});
+
+// 2
+test("Replaces a Custom Element's DOM with a VDOM", function (t) {
+  t.plan(2);
+  let h = bondo.h;
+  bondo('el-two', function () {
+    return h('div#new-two', "New Two");
+  });
+  //console.dir(document.getElementById('old-two'));
+  //console.dir(document.getElementById('new-two'));
+  t.equal(document.getElementById('old-two'), null);
+  t.equal(document.getElementById('new-two').tagName, 'DIV');
+});
+
+/*
+var test = require('tape');
 test('binds model properties to attributes', function (t) {
   t.plan(1);
   var el = bondo('testOne', {username: 'user1'});
@@ -18,3 +42,4 @@ test('when model properties change, attributes change', function (t) {
     t.equal(el.querySelector('input').value, 'user3');
   }, 0); 
 });
+*/
